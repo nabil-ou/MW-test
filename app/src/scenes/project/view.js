df1f8ba1b9615cabaaa9f0b333bfa15d8c1f23b0
nabil-ou <nabil.outaous@gmail.com>
2025-04-09 16:11:46 +0200

    fix: update project data handling and improve link normalization

--- app/src/scenes/project/view.js
+++ app/src/scenes/project/view.js
@@ -32,7 +32,7 @@ export default function ProjectView() {
   useEffect(() => {
     (async () => {
       const { data: u } = await api.get(`/project/${id}`);
-      setProject(u);
+      setProject(Array.isArray(u) ? u[0] : u);
     })();
   }, []);
 
@@ -71,7 +71,6 @@ export default function ProjectView() {
 }
 
 const ProjectDetails = ({ project }) => {
-  console.log(project);
   return (
     <div>
       <div className="flex flex-wrap p-3">
@@ -80,7 +79,7 @@ const ProjectDetails = ({ project }) => {
             <div className="w-full">
               <div className="flex justify-between gap-2">
                 <div className="flex gap-20">
-                  <span className="w-fit text-[20px] text-[#0C1024] font-bold">Nom du projet : </span>
+                  <span className="w-fit text-[20px] text-[#0C1024] font-bold">Project name : </span>
                   <span className="w-fit text-[20px] text-[#0C1024] font-bold">{project.name}</span>
                 </div>
                 <div className="flex flex-1 flex-column items-end gap-3">
@@ -89,6 +88,7 @@ const ProjectDetails = ({ project }) => {
               </div>
               <div className="w-full md:w-[50%]">
                 <div className="pt-2 ">
+                  <span className="w-fit text-[20px] text-[#0C1024] font-bold">Description : </span>
                   <span className="text-[16px] text-[#676D7C] font-medium">{project.description ? project.description : ""}</span>
                 </div>
                 <div className="mt-4 text-[18px] text-[#000000] font-semibold">
@@ -272,6 +272,12 @@ const Field = ({ value = "-", ...rest }) => {
 };
 
 const Links = ({ project }) => {
+  const normalizeUrl = (url) => {
+    if (!url.startsWith("http")) {
+      return `https://${url}`;
+    }
+    return url;
+  };
   return (
     <div className="flex flex-wrap gap-3">
       {project.website && (
@@ -286,7 +292,7 @@ const Links = ({ project }) => {
       )}
       {project.links?.map((link) => (
         <div key={link.url} className="group text-sm font-medium	text-blue-700 border-[1px] border-blue-700 rounded-full overflow-hidden">
-          <a target="blank" href={link.url} className="break-words cursor-pointer text-blue-700 hover:text-white hover:bg-blue-700 flex hover:no-underline h-full">
+          <a target="blank" href={normalizeUrl(link.url)} className="break-words cursor-pointer text-blue-700 hover:text-white hover:bg-blue-700 flex hover:no-underline h-full">
             <div className="flex items-center bg-blue-700 py-1 px-2 rounded-r-full ">
               <IoIosLink className="group-hover:scale-110 text-white" />
             </div>
